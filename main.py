40905ae6ed0e1b50b0cdceca494b4b550f59f38f
Your Name <you@example.com>
2023-11-12 20:50:12 +0100

    final final commit

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from datetime import datetime, timedelta
 
 estaciones_detalles = pd.read_csv("CSV/2022_MeteoCat_Detall_Estacions.csv")
 estaciones = pd.read_csv("CSV/2020_MeteoCat_Estacions.csv")
